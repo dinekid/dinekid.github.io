c0bc4c9240e0fb0d7adaa19da7c73e326d817580
dinekid <tysan98121@maricopa.edu>
2024-04-17 11:42:37 -0700

    Exercise 19

--- HelloObject.js
+++
@@ -1,6 +0,0 @@
-let user = {}; // Create an empty object user.
-user.name = "Tyson"; // Add the property name with a made up name as the value.
-user.lastName = "Evans"; // Add the property lastName with a made up last name as the value.
-user.userName = "Tysan98121"; // Add the property userName with a made up username as the value.
-user.name = "Delford"; // Change the value of the name to a different name.
-delete user.lastName; // Remove the property lastName from the object.
