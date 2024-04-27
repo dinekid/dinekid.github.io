9cbaf8a11aa4676b174bec6bedc5e699c184155b
dinekid <tysan98121@maricopa.edu>
2024-04-27 10:21:37 -0700

    fix6

--- main3.js
+++ main3.js
@@ -1,38 +1,32 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-const images = ['Helldivers2.jpg', 'MagicCom.jpg', 'paintingmini.jpg', 'RallyRace.jpg', 'role-playing-games.jpeg'];
+const images = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 const alts = {
-    'Helldivers2.jpg': 'Squad just looking awesome',
-    'MagicCom.jpg': 'some folks sitting around a table looking happy but soon to be mad',
-    'paintingmini.jpg': 'Someone painting there stress and troubles away',
-    'RallyRace.jpg': 'Something i enjoy watching on TV, but rather try once',
-    'role-playing-games.jpeg': 'Folks around the table telling an epic story'
+  'images/pic1.jpg': 'Squad just looking awesome',
+  'images/pic2.jpg': 'some folks sitting around a table looking happy but soon to be mad',
+  'images/pic3.jpg': 'Someone painting their stress and troubles away',
+  'images/pic4.jpg': 'Something I enjoy watching on TV, but would rather try once',
+  'images/pic5.jpg': 'Folks around the table telling an epic story'
 };
 
 for (const image of images) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${images}`);
-    newImage.setAttribute('alt', alts[image]);
-    thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', e => {
-      displayedImage.src = e.target.src;
-      displayedImage.alt = e.target.alt;
-    });
+  const newImage = document.createElement('img');
+  newImage.src = `images/${image}`;
+  newImage.alt = alts[image];
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
 }
-  
+
 btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class');
-    if (btnClass === 'dark') {
-        btn.setAttribute('class','light');
-        btn.textContent = 'Lighten';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else {
-        btn.setAttribute('class','dark');
-        btn.textContent = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    }
+  const btnClass = btn.classList.contains('dark') ? 'light' : 'dark';
+  btn.classList.toggle('dark');
+  btn.classList.toggle('light');
+  btn.textContent = btnClass === 'dark' ? 'Darken' : 'Lighten';
+  overlay.style.backgroundColor = btnClass === 'dark' ? 'rgba(0,0,0,0.5)' : 'rgba(0,0,0,0)';
 });
