ad19da62a06d52bc820dea43c2446c2cb005fc26
dinekid <tysan98121@maricopa.edu>
2024-04-27 09:25:42 -0700

    next fix

--- main3.js
+++ main3.js
@@ -1,31 +1,38 @@
-const imageFilenames = ['images/Helldivers2.jpg', 'images/MagicCom.jpg', 'images/paintingmini.jpg', 'images/RallyRace.jpg', 'images/role-playing-games.jpeg'];
+const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
-imageFilenames.forEach(filename => {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', filename);
-    newImage.setAttribute('alt', 'Image');
-    thumbBar.appendChild(newImage);
-
-    newImage.addEventListener('click', function() {
-        const src = this.getAttribute('src');
-        const alt = this.getAttribute('alt');
-        document.querySelector('.displayed-img').setAttribute('src', src);
-        document.querySelector('.displayed-img').setAttribute('alt', alt);
-    });
-});
-
-const btn = document.querySelector('.dark');
+const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-btn.addEventListener('click', function() {
-    if (this.getAttribute('class') === 'dark') {
-        this.setAttribute('class', 'light');
-        this.textContent = 'Lighten';
-        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+const images = ['images\Helldivers2.jpg', 'images\MagicCom.jpg', 'images\paintingmini.jpg', 'images\RallyRace.jpg', 'images\role-playing-games.jpeg'];
+const alts = {
+    'images\Helldivers2.jpg': 'Squad just looking awesome',
+    'images\MagicCom.jpg': 'some folks sitting around a table looking happy but soon to be mad',
+    'images\paintingmini.jpg': 'Someone painting there stress and troubles away',
+    'images\RallyRace.jpg': 'Something i enjoy watching on TV, but rather try once',
+    'images\role-playing-games.jpeg': 'Folks around the table telling an epic story'
+};
+
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images\${images}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+}
+  
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
     } else {
-        this.setAttribute('class', 'dark');
-        this.textContent = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
 });
